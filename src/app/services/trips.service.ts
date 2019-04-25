ebf1bb7c1279b2a4a4a9ba7326772d90fc090424
Muhammad Kamran <m4kamran008@gmail.com>
2019-04-25 16:22:35 +0500

    helper functions

--- src/app/services/trips.service.ts
+++ src/app/services/trips.service.ts
@@ -1,42 +1,78 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
 import { firestore } from 'firebase';
 
 @Injectable()
 export class TripService {
+  tripCollection: AngularFirestoreCollection<any>;
+  trips: Observable<any>;
+  constructor(public afs: AngularFirestore) {
+    console.log('Trip service instantiated...');
+  }
+  getTripDetails() {
+    return this.afs
+      .collection('trips')
+      .snapshotChanges()
+      .pipe(
+        map(res => {
+          return res.map(data => {
+            return { id: data.payload.doc.id, data: data.payload.doc.data() };
+          });
+        })
+      );
+  }
+  tripDetails(tripId) {
+    return this.afs
+      .collection('trips')
+      .doc(tripId)
+      .snapshotChanges()
+      .pipe(
+        map(data => {
+          return {
+            id: data.payload.id,
+            data: data.payload.data()
+          };
+        })
+      );
+  }
+  addHotelToTrip(hotelId, tripId) {
+    console.log(`hoteldId: ${hotelId}  |tripId: ${tripId}`);
+    return this.afs
+      .collection('trips')
+      .doc(tripId)
+      .set(
+        { hotels: firestore.FieldValue.arrayUnion(hotelId) },
+        { merge: true }
+      );
+  }
 
+  addResturantToTrip(resturantId, tripId) {
+    console.log(`hoteldId: ${resturantId}  |tripId: ${tripId}`);
+    return this.afs
+      .collection('trips')
+      .doc(tripId)
+      .set(
+        { resturants: firestore.FieldValue.arrayUnion(resturantId) },
+        { merge: true }
+      );
+  }
 
-    tripCollection: AngularFirestoreCollection<any>;
-    trips: Observable<any>;
-    constructor(public afs: AngularFirestore) 
-    {
-        console.log("Trip service instantiated...");
-    }
-    getTripDetails() 
-    {
-            return this.afs.collection('trips').snapshotChanges().pipe(map(res => {
-            return res.map(data => { return { id: data.payload.doc.id, data: data.payload.doc.data() } })
-        }))
-    }
-    tripDetails(tripId)
-    {
-        return this.afs.collection('trips').doc(tripId).snapshotChanges().pipe(map(data => {
-            return {
-                id: data.payload.id, data: data.payload.data()
-            }
-        }))        
-    }
-    addHotelToTrip(hotelId, tripId) 
-    {
-        console.log(`hoteldId: ${hotelId}  |tripId: ${tripId}`);
-        return this.afs.collection('trips').doc(tripId).set({ 'hotels': firestore.FieldValue.arrayUnion(hotelId) }, { merge: true });
-    }
+  showPublicTrips() {
+    this.tripCollection = this.afs.collection('trips', ref =>
+      ref.where('status', '==', 'public')
+    );
+    return this.tripCollection.valueChanges();
+  }
 
-    addResturantToTrip(resturantId, tripId)
-        {
-            console.log(`hoteldId: ${resturantId}  |tripId: ${tripId}`);
-            return this.afs.collection('trips').doc(tripId).set({ 'resturants': firestore.FieldValue.arrayUnion(resturantId) }, { merge: true });
-        }
+  showPrivateTrips(id: string) {
+    this.tripCollection = this.afs.collection('trips', ref =>
+      ref.where('userId', '==', id)
+    );
+    return this.tripCollection.valueChanges();
+  }
 }
